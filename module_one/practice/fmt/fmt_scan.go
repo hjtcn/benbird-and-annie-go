63fc70e996ee3132ed8a3864cd930f1a01aa1c06
jiangtao <heijiangtao@51shebao.com>
2021-12-01 20:03:19 +0800

    完善计算圆面积文案

--- module_one/practice/fmt/fmt_scan.go
+++ module_one/practice/fmt/fmt_scan.go
@@ -11,10 +11,10 @@ func main()  {
 	var r_one float64
 	var r_two float64
 
-	fmt.Println("输入第一个圆的半径")
+	fmt.Println("输入第一个圆的半径:")
 	fmt.Scan(&r_one)
 
-	fmt.Println("输入第二个圆的半径")
+	fmt.Println("输入第二个圆的半径:")
 	fmt.Scan(&r_two)
 
 	fmt.Println("两圆面积和:", r_one * r_one * pai + r_two * r_two * pai)
