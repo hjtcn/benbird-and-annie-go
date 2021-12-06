d617503c3e36be2097e27aa847ad85e6b9ee9331
BenBird <hjt1160986096@gmail.com>
2021-12-06 23:06:33 +0800

    单个人体脂率计算

--- module_one/combat/upgraded_fat_mass.go
+++ module_one/combat/upgraded_fat_mass.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func suggestTemp(fatRateMsg string) string {
+func suggest(fatRateMsg string) string {
 	switch fatRateMsg {
 	case "体脂率有误":
 		return "请检验个人信息是否输入正确"
@@ -21,24 +21,23 @@ func suggestTemp(fatRateMsg string) string {
 	}
 }
 
-func fatMass() {
-	var weight float64 = 58
-	var tall float64 = 1.63
-	var bmi float64 = weight / (tall * tall)
-
-	var age int = 24
-	var sexWeight int
-	var sex string = "女"
-
+func getSexWeight(sex string) int {
 	if sex == "男" {
-		sexWeight = 1
-	} else {
-		sexWeight = 0
+		return 1
 	}
 
-	var fatRate float64 = (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
-	fmt.Println("我的体脂率:", fatRate)
+	return 0
+}
 
+func getBmi(weight, tall float64) float64 {
+	return weight / (tall * tall)
+}
+
+func getFatRate(bmi float64, age, sexWeight int) float64 {
+	return (1.2*bmi + 0.23*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
+}
+
+func getFatRateResult(fatRate float64, age, sexWeight int) string {
 	fatRateMsg := "没什么建议"
 	if sexWeight == 1 {
 		if age >= 18 && age <= 39 {
@@ -130,7 +129,83 @@ func fatMass() {
 		}
 	}
 
-	suggestInfo := suggestTemp(fatRateMsg)
-
-	fmt.Println(suggestInfo)
+	return fatRateMsg
 }
+
+func main() {
+	var goOn string = "yes"
+
+	for goOn == "yes" {
+		var weight float64
+		var tall float64
+		var age int
+		var sex string
+
+		fmt.Print("请输入您的年龄(岁): ")
+		fmt.Scanf("%d", &age)
+
+		for age < 18 {
+			fmt.Print("只能计算18岁以上的呦，请再次输入年龄(岁): ")
+			fmt.Scanf("%d", &age)
+		}
+
+		fmt.Print("请输入您的性别(男/女): ")
+		fmt.Scanf("%s", &sex)
+
+		for sex != "男" && sex != "女" {
+			fmt.Print("请输入有效的性别(男/女): ")
+			fmt.Scanf("%s", &sex)
+		}
+
+		fmt.Print("请输入您的体重(公斤): ")
+		fmt.Scanf("%f", &weight)
+
+		for weight < 30 {
+			fmt.Print("请输入您的有效体重(公斤): ")
+			fmt.Scanf("%f", &weight)
+		}
+
+		fmt.Print("请输入您的身高(米): ")
+		fmt.Scanf("%f", &tall)
+
+		for tall < 0.5 || tall > 3.0 {
+			fmt.Print("请输入您的身高(米): ")
+			fmt.Scanf("%f", &tall)
+		}
+
+		sexWeight := getSexWeight(sex)
+		bmi := getBmi(weight, tall)
+		fatRate := getFatRate(bmi, age, sexWeight)
+
+		fatRateMsg := getFatRateResult(fatRate, age, sexWeight)
+		suggestInfo := suggest(fatRateMsg)
+
+		fmt.Println("我的体脂率:", fatRate)
+		fmt.Println(suggestInfo)
+
+		fmt.Println("")
+		fmt.Print("请问您是否还要继续计算体脂率(yes/no): ")
+		fmt.Scanf("%s", &goOn)
+
+		err_num := 1
+		for goOn != "yes" && goOn != "no" {
+			if err_num > 3 {
+				break
+			}
+			fmt.Print("请问您是否还要继续计算体脂率(yes/no): ")
+			fmt.Scanf("%s", &goOn)
+			err_num++
+		}
+
+		if goOn == "no" {
+			break
+		}
+	}
+
+	fmt.Println("已退出体脂计算！\n")
+}
+
+/**
+实现单个人的体脂率计算
+但是存在的问题是：输入信息时，如果输入的值类型不是定义的类型，则Scanf阻塞异常
+ */
