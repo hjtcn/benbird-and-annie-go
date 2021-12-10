1382f38996fc67d09b6a4ee1186adfdb89df3211
BenBird <hjt1160986096@gmail.com>
2021-12-10 08:40:38 +0800

    函数模块随堂笔记

---
+++ module_two/practice/common/func_test.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"testing"
+	"time"
+)
+
+func TestFunc(t *testing.T) {
+	startTime := time.Now()
+	defer func() {
+		finishTime := time.Now()
+		t.Log("运行时间：", finishTime.Sub(startTime))
+	}()
+	arr := make([]int, 1, 100)
+	t.Log(arr, len(arr), cap(arr))
+
+	arr1 := []int{}
+	res := copy(arr1, arr)
+
+	t.Log(res)
+
+	arr = append(arr, 2)
+	t.Log(arr, len(arr), cap(arr))
+
+
+	t.Log(arr1, len(arr1), cap(arr1))
+}
