542c917ed1dc2d4f9c39d794ec73897fcfb00f92
BenBird <hjt1160986096@gmail.com>
2022-03-07 08:30:11 +0800

    模块十

--- module_ten/gorm_mysql/main.go
+++ module_ten/gorm_mysql/main.go
@@ -1,10 +1,28 @@
 package main
 
 import (
-	_ "gorm.io/driver/mysql"
-	_ "gorm.io/gorm"
+	"github.com/hjtcn/benbird-and-annie-go/pkg/apis"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 )
 
+func connectDb() (db *gorm.DB) {
+	dsn := "root:123456@tcp(127.0.0.1:3306)/go_learning?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("数据库连接失败")
+	}
+
+	return db
+}
+
+func selectDb(db *gorm.DB) {
+	res := []*apis.PersonalInfo{}
+}
+
 func main() {
+	db := connectDb()
 
 }
